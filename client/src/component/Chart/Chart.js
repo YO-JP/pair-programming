fc8f96c0c2c117a674d7281babb4afed6deeb364
Yuta Obara <o.yuta722@gmail.com>
2020-02-28 09:37:31 -0500

    version 3

--- client/src/component/Chart/Chart.js
+++ client/src/component/Chart/Chart.js
@@ -1,73 +1,23 @@
 import React from 'react';
-import Plot from 'react-plotly.js';
-//https://plot.ly/javascript/react/
-//https://www.alphavantage.co/documentation/
+
+import ChartOne from '.././ChartIndividual/ChartOne.js';
+import './Chart.scss'
 
 class Chart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ChartX: [],
-      ChartY: []
+    state ={
+        visible:false
     }
-  }
-
-  componentDidMount() {
-    this.fetchStock();
-  }
-
-  fetchStock() {
-    const This = this;
-    console.log(This);
-    const API_KEY = 'KUJ2BOOFVHWXUA63';
-    let StockSymbol = 'MSFT';
-    let API = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
-    let ChartXFunction = [];
-    let ChartYFunction = [];
-
-    fetch(API)
-      .then(
-        function(response) {
-          return response.json();
-        }
-      )
-      .then(
-        function(data) {
-          console.log(data);
-
-          for (var key in data['Monthly Adjusted Time Series']) {
-            ChartXFunction.push(key);
-            ChartYFunction.push(data['Monthly Adjusted Time Series'][key]['1. open']);
-          }
-
-          // console.log(ChartXFunction);
-          This.setState({
-            ChartX: ChartXFunction,
-            ChartY: ChartYFunction
-          });
-        }
-      )
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Stock Market</h1>
-        <Plot
-          data={[
-            {
-              x: this.state.ChartX,
-              y: this.state.ChartY,
-              type: 'scatter',
-              mode: 'lines+markers',
-              marker: {color: 'red'},
-            }
-          ]}
-          layout={{width: 720, height: 500, title: 'Stock Market'}}
-        />
-      </div>
-    )
-  }
+    render(){
+        return (
+            <div className="chart">
+                <button className = "chart__button" onClick={()=>{
+                    this.setState({visible:!this.state.visible});
+                }}>CLICK ME!</button>
+                {this.state.visible ? <ChartOne /> : null}
+    
+            </div>
+        )
+    }
 }
 
-export default Chart;
+export default Chart
